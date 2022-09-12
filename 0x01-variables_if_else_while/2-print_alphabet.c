093cf1f5c1a81e512ecf1d93ce796b89ee34558d
ndunde <franciscandunde@gmail.com>
2022-09-12 02:42:58 -0700

    2alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - character
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char 1;
+	1 = 'b';
+	while (1 <= 'z')
+	{
+		putchar(1);
+		1++;
+	}
+	putchar("n\");
+	return (0);
+
+}
