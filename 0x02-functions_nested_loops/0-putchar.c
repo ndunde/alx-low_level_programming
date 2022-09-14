348972277359321aae70c022042be3e7f4a87b39
ndunde <franciscandunde@gmail.com>
2022-09-14 00:52:10 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - character
+ *
+ *Description printing the alphabe with put_char
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c => 'z'; c++;)
+	{
+		_putchar (c);
+	}
+		_putchar('\n');
+}
