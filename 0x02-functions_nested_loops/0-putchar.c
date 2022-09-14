0f9fa5302c0febdabe437eddfbccea0c042de35a
ndunde <franciscandunde@gmail.com>
2022-09-14 03:55:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,17 +4,18 @@
  * main - print_alphabet character within a loop
  * Description:print the alphabet with put_char
  *
- * Return: on sucess 1
- * on error ,-1 is returned and errno is set appropriat
+ * Return: always 0 sucess
  */
-int void print_alphabet(void)
+void main(print_alphabet)
+
 {
 	char c;
 
 	for (c = 'a'; c => 'z'; c++)
 	{
-	putchar(c);
-	print implicit_function declaration _putchar('\n');
+	putchar(c)
+		_putchar('\n');
 	}
-	return -1
+
+	return(0)
 }
